69202211da5ef824c34d5ba1c2449515f669d19a
gbhaynes3 <gifford@ashfordsoftware.com>
2012-07-03 21:48:11 -0500

    Initial commit
    Created base Domain objects and some projects for FindMyLibrary.
    Moved to VS 2012

---
+++ src/app/FindMyLibrary.Domain/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMyLibrary.Domain
+{
+  public class Address
+  {
+    public string Line1 { get; set; }
+    public string City { get; set; }
+    public State State { get; set; }
+    public string Zip { get; set; }
+
+  }
+}
---
+++ src/app/FindMyLibrary.Domain/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMyLibrary.Domain
+{
+  public class Location
+  {
+    public float Longitude { get; set; }
+    public float Latitude { get; set; }
+  }
+}
