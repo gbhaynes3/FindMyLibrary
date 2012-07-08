01feedd9d90c04ad0a716cf0b7b3931bda9289c9
gbhaynes3 <gifford@ashfordsoftware.com>
2012-07-07 22:22:23 -0500

    added CityControllerTest

---
+++ src/test/FindMyLibrary.Controllers.UnitTests/CityControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMyLibrary.Controllers.UnitTests
+{
+  class CityControllerTests
+  {
+  }
+}
