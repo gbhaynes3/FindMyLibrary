dff4b150a796fc161cdfed2fc668845650992046
Gifford Haynes <gifford.haynes@gmail.com>
2012-07-26 16:53:37 -0500

    Added Database Project

---
+++ src/app/FindMyLibrary.Database/dbo/Tables/Libraries.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Libraries] (
+    [Id]                INT            IDENTITY (1, 1) NOT NULL,
+    [Name]              NVARCHAR (MAX) NULL,
+    [Phone]             NVARCHAR (MAX) NULL,
+    [Latitue]           FLOAT (53)     NOT NULL,
+    [Longitude]         FLOAT (53)     NOT NULL,
+    [Address_AddressId] INT            NULL,
+    CONSTRAINT [PK_Libraries] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Libraries_Addresses_Address_AddressId] FOREIGN KEY ([Address_AddressId]) REFERENCES [dbo].[Addresses] ([AddressId])
+);
+
+
+GO
+ALTER TABLE [dbo].[Libraries] NOCHECK CONSTRAINT [FK_Libraries_Addresses_Address_AddressId];
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Address_AddressId]
+    ON [dbo].[Libraries]([Address_AddressId] ASC);
+
---
+++ src/app/FindMyLibrary.Database/dbo/Tables/States.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[States] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Abbreviation] NVARCHAR (MAX) NULL,
+    [Name]         NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_States] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
